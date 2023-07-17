74f505b3c322ccac81e7335bfc403b92a54d49dd
Williamatthewood <w.wood@tcu.edu>
2023-07-17 14:26:48 -0500

    project information displayed in cards, more styling still needed

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 export default function Project(props) {
     return props.projects.map((project, i) => (
-        <div key={i}>
-            <div key={project.id}>
-                <p>{project.name}</p>
+            <div key={project.id} className="card col-md-5 col-lg-3 m-4 ">
+                <h4 className="card-title">{project.name}</h4>
+                <img src={project.image} className="card-img-top" alt="application thumbnail" />
+                <div className="card-body">
+                    <a href={project.appLink} className="card-link">Application</a>
+                    <a href={project.gitHubLink} className="card-link">GitHub</a>
+                </div>
             </div>
-        </div>
     ))
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -8,49 +8,52 @@ export default function Portfolio() {
       name: 'Dinner and a Movie',
       appLink: 'https://mvincent15.github.io/Dinner-and-a-Movie/',
       gitHubLink: 'https://github.com/Williamatthewood/Dinner-and-a-Movie',
-      image: '../../../public/images/dinnerAndAMovie.png'
+      image: './images/dinnerAndAMovie.png'
     },
     {
       id: 1,
       name: 'furfacebook',
       appLink: 'https://furfacebook-app.herokuapp.com/',
       gitHubLink: 'https://github.com/Williamatthewood/FurFaceBook',
-      image: '../../../public/images/furfacebook.png'
+      image: './images/furfacebook.png'
     },
     {
       id: 2,
       name: 'Password Generator',
       appLink: 'https://williamatthewood.github.io/Password-Generator-Challenge/',
       gitHubLink: 'https://github.com/Williamatthewood/Password-Generator-Challenge',
-      image: '../../../public/images/passwordGenerator.png'
+      image: './images/passwordGenerator.png'
     },
     {
       id: 3,
       name: 'Coding Quiz',
       appLink: 'https://williamatthewood.github.io/Code-Quiz-Challenge/',
       gitHubLink: 'https://github.com/Williamatthewood/Code-Quiz-Challenge',
-      image: '../../../public/images/codingQuiz.png'
+      image: './images/codingQuiz.png'
     },
     {
       id: 4,
       name: 'Work Day Scheduler',
       appLink: 'https://williamatthewood.github.io/Work-Day-Scheduler-Challenge/',
       gitHubLink: 'https://github.com/Williamatthewood/Work-Day-Scheduler-Challenge',
-      image: '../../../public/images/workDayScheduler.png'
+      image: './images/workDayScheduler.png'
     },
     {
       id: 5,
       name: 'Weather Dashboard',
       appLink: 'https://williamatthewood.github.io/Weather-Dashboard-Challenge/',
       gitHubLink: 'https://github.com/Williamatthewood/Weather-Dashboard-Challenge',
-      image: '../../../public/images/weatherDashboard.png'
+      image: './images/weatherDashboard.png'
     },
     
   ]
   return (
     <div>
-      <h1>Portfolio</h1>
-      <Project projects = {projects} />
+      <h1 className="d-flex flex-start">Projects</h1>
+      <div className="row d-flex justify-content-center">
+        <Project projects = {projects} />
+      </div>
+      
     </div>
   );
 }
