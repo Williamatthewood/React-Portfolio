051119eac16bf3c4a8303f6fa857c1924b538934
Williamatthewood <w.wood@tcu.edu>
2023-07-19 15:30:45 -0500

    worked ok footer some

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
     return (
-      <footer className="footer">
+      <footer className="footer my-4 text-center">
         <a className="m-3" href="https://github.com/Williamatthewood" target="_blank">GitHub</a>
         <a className="m-3" href="https://www.linkedin.com/in/williamatthewood/" target="_blank">LinkedIn</a>
       </footer>
